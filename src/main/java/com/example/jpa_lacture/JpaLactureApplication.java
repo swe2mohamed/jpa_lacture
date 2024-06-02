420ed1cd315513086b6026c27c8c963491adacb4
swe2mohamed <swe.mohamed1990@gmail.com>
2024-06-02 09:54:37 +0200

    Initial commit

---
+++ src/main/java/com/example/jpa_lacture/JpaLactureApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_lacture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLactureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLactureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpa_lacture/JpaLactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_lacture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLactureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
